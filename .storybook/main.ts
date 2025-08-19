9644973c04e5ec0730b0650650030c6193ebf0b9
Kris Siepert <m@kris.cool>
2025-08-19 16:14:29 +0200

    feat: add additional components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,5 +12,8 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
